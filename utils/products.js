3137136f33c2afaff4a7b74368cfb32cb4197f91
Khalid <khalid@Macshops-JNFVTKW9CQ.local>
2026-05-23 18:04:16 +0500

    done

--- utils/products.js
+++ utils/products.js
@@ -32,7 +32,9 @@ const getProducts = (url) => {
 
 getProducts(API);
 
-function ShowProducts(data) {
+function ShowProducts(data, newData) {
+  console.log(newData);
+
   data.forEach((element) => {
     const { description, title, category, id, image, price } = element;
     tBody.innerHTML += `
@@ -62,3 +64,51 @@ function ShowProducts(data) {
     `;
   });
 }
+
+//addProducts
+
+const elForm = document.querySelector(".form");
+
+elForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  const image = elForm["image"].value.trim();
+  const title = elForm["title"].value.trim();
+  const price = elForm["price"].value.trim();
+  const category = elForm["category"].value.trim();
+  const description = elForm["tavsif"].value.trim();
+
+  const newPro = {
+    image: image,
+    title: title,
+    price: price,
+    category: category,
+    description: description,
+  };
+
+  fetch(API, {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify(newPro),
+  })
+    .then((res) => res.json())
+    .then((data) => {
+      console.log(data);
+
+      Toastify({
+        text: "Successful added products",
+        duration: 3000,
+        destination: "https://github.com/apvarun/toastify-js",
+        newWindow: true,
+        close: true,
+        gravity: "top", // `top` or `bottom`
+        position: "right", // `left`, `center` or `right`
+        stopOnFocus: true, // Prevents dismissing of toast on hover
+
+        style: {
+          background: "linear-gradient(to right, #188c09, #58a514)",
+        },
+        onClick: function () {}, // Callback after click
+      }).showToast();
+    });
+});
